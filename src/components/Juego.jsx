6af04e5c9ea9200f99de487dfb66e95070d4cf2d
Sara VALENTINA Benavides <castrobenavidesarmysaravalenti@gmail.com>
2025-11-13 14:51:57 -0500

    Quite la linea género de juego

--- src/components/Juego.jsx
+++ src/components/Juego.jsx
@@ -3,7 +3,6 @@ export default function Juego({ nombre, genero, icon }) {
   return (
     <div className="juego-item">
       <h3>{icon} {nombre}</h3>
-      <p><strong>Género:</strong> {genero || "No especificado"}</p>
     </div>
   );
 }
